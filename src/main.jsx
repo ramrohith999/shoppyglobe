62cf905a42b4b8f13a084fa8f33ea29f2b3215ea
Ram Rohith Maringanti <ramrohith999@gmail.com>
2026-05-23 11:40:12 +0530

    fixed app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );

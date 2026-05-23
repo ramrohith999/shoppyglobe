7763b9eced962f9b018f5e2c643dc0eda5b0805d
Ram Rohith Maringanti <ramrohith999@gmail.com>
2026-05-23 15:16:20 +0530

    fixed ui for searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,18 +4,20 @@ import { setSearchQuery } from "../redux/searchSlice";
 const SearchBar = () => {
   const dispatch = useDispatch();
 
-  const query = useSelector((state) => state.search.query);
+  const query = useSelector(
+    (state) => state.search.query
+  );
 
   return (
-    <div className="mb-6">
+    <div className="mb-8">
       <input
         type="text"
-        placeholder="Search products..."
+        placeholder="Search for what you need..."
         value={query}
         onChange={(e) =>
           dispatch(setSearchQuery(e.target.value))
         }
-        className="w-full border border-gray-300 rounded-lg p-3 outline-none focus:ring-2 focus:ring-blue-500"
+        className="w-full bg-white shadow-md border border-gray-200 p-4 outline-none focus:ring-4 focus:ring-blue-300 transition rounded-4xl"
       />
     </div>
   );
